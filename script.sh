209ad939efc46f2e88569d94966de6c14fd71f9e
Lukas M <lukas.mrtvy@gmail.com>
2020-06-01 20:21:08 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -117,49 +117,10 @@ _getProjectsToChange () {
     done
   fi
 
-   if (( "${#pnames[@]}" > 0 )); then
-    if [ "$action" == "validate" ]; then
-       for i in ${#pnames[@]}; do
-          TERRAGRUNT_DISABLE_INIT="true" terragrunt validate --terragrunt-working-dir "${paths[i]}"
-       done
-    elif [ "$action" == "plan" ]; then
-      # TODO: loop over unique logins
-      readarray -td '' umethods< <(printf '%s\0' "${pmethods[@]}" | LC_ALL=C sort -zu)
-      for m in "${umethods[@]}"; do
-       _cliLogin "$m"
-      done
-      for i in ${#pnames[@]}; do
-         terragrunt plan --terragrunt-working-dir "${paths[i]}"
-      done
-      for m in "${umethods[@]}"; do
-       _cliLogout "$m"
-      done
-    elif [ "$action" == "apply" ]; then
-      readarray -td '' umethods< <(printf '%s\0' "${pmethods[@]}" | LC_ALL=C sort -zu)
-      for m in "${umethods[@]}"; do
-       _cliLogin "$m"
-      done
-      for i in ${#pnames[@]}; do
-         terragrunt apply --auto-apply --terragrunt-working-dir "${paths[i]}"
-      done
-      for m in "${umethods[@]}"; do
-       _cliLogout "$m"
-      done
-    elif [ "$action" == "destroy" ]; then
-      readarray -td '' umethods< <(printf '%s\0' "${pmethods[@]}" | LC_ALL=C sort -zu)
-      for m in "${umethods[@]}"; do
-       _cliLogin "$m"
-      done
-      for i in ${#pnames[@]}; do
-         terragrunt destroy --auto-apply --terragrunt-working-dir "${paths[i]}"
-      done
-      for m in "${umethods[@]}"; do
-       _cliLogout "$m"
-      done
-    fi
+  if (( "${#pnames[@]}" > 0 )); then
+     echo "A"
   elif (( "${#pnames[@]}" == 0 )); then
-    echo "no changes"
-    exit 0
+     echo "B"
   fi
 
 }
